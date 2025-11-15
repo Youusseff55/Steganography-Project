6015f764200b10f969a73a72427c9841e83729e4
Youusseff55 <Youusseff55@icloud.com>
2025-11-15 15:36:31 +0200

    Update Steganography_Code.py

--- Steganography_Code.py
+++ Steganography_Code.py
@@ -1,2 +1,12 @@
-# Just testing github repo
-# Just testing github repo #2
+def stega_encode(message, image_path=0):
+
+# transforms the message to be hidden into binary format that can be hidden in the image
+    binary_message=''
+    for ch in message:
+        ascii_format=ord(ch)
+        binary_ch=format(ascii_format,'08b')
+        binary_message+=binary_ch
+    return binary_message
+
+
+print(stega_encode("hi"))
