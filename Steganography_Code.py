023f29e6cc9857aa32708957f2d99f86cf9f4ccf
Youusseff55 <Youusseff55@icloud.com>
2025-11-15 16:13:31 +0200

    image added and converting it to a binary list in the code done

--- Steganography_Code.py
+++ Steganography_Code.py
@@ -1,12 +1,32 @@
-def stega_encode(message, image_path=0):
+def stega_encode(message, image_path):
 
 # transforms the message to be hidden into binary format that can be hidden in the image
+
     binary_message=''
+    final_binary_list=[]
+
     for ch in message:
         ascii_format=ord(ch)
         binary_ch=format(ascii_format,'08b')
         binary_message+=binary_ch
-    return binary_message
+    print(f"your secret message have been converted to binary and it's : {binary_message}")
+
+#convert image bytes to binary list
+
+    with open (image_path, 'rb') as image_file:
+            byte_data=image_file.read()
+            byte_list=list(byte_data)
+            for byte in byte_list:
+                binary_byte = format(byte,"08b")
+                final_binary_list.append(binary_byte)
+            print (final_binary_list)
+
+stega_encode("hi","boat.png")
 
 
-print(stega_encode("hi"))
+    
+
+
+
+
+        
